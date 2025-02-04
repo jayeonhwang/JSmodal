7e7805c37e92ce8c67fc62bb8930b07c89a892c6
Jayeon Hwang <jayeon512@gmail.com>
2025-02-04 11:36:33 -0600

    store function for open and close modal

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
-
-
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
